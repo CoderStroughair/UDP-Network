231fa444da9b376ec2ea3de374a628f27abb1e76
Michael2 <stroughm@tcd.ie>
2015-05-12 11:13:39 -0700

    Removed unnecessary files, and brought the code back to a simple UDP format, no extras

--- server.c
+++ server.c
@@ -1,140 +1,59 @@
 #pragma GCC diagnostic ignored "-Wpointer-to-int-cast"
 #pragma GCC diagnostic ignored "-Wint-to-pointer-cast"
 
-#include<stdio.h>
-#include<string.h>
-#include<stdlib.h>
-#include<arpa/inet.h>
-#include<sys/socket.h>
- 
-#define PORT 8888   	//The port on which to listen for incoming data
-#define BUFLEN 1024		//Max length of buffer
-#define MESSAGESIZE 8	//size of the message
-#define	PACKSIZE	15
-#define HEADER		2
-#define TRAILER		4 
-int nextpacketnumber = 1;
-void die(char *s)
-{
-    perror(s);
-    exit(1);
-}
+/************* UDP SERVER CODE *******************/
 
+#include <stdio.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
 
-int convertpacket(char message[], char packet[])
-{
-	if ((int)packet[0] == '9')
-	{
-		printf("Recieved last packet\n");
-		return -1;
-	}
-	char buffer[MESSAGESIZE];
-	unsigned int i, j;
-	j = (int)packet[1] - '0';		// to hold the order of the packet
-	for (i = 0; i<MESSAGESIZE; i++)
-	{
-		buffer[i] = packet[i+HEADER];
-	}
-	printf("%s\n", buffer);
-	if (i = (int)buffer%2 ==(int)(packet[MESSAGESIZE+HEADER]-'0'))
-		{
-			//printf("Passes CRC check\n");
-		}
-	else
-	{
-		printf("CRC Fails\n");
-		return j;
-	}
+/************ ASCII Conversion Table ************/
+char ASCII [16][6] = {			
+		{' ','!','"','#','$','%','&',''','(',')','*','+',',','-','.','/'}, //[x][0] - 2.x
+		{'0','1','2','3','4','5','6','7','8','9',':',';','<','+','>','?'}, //[x][1] - 3.x
+		{'@','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'}, //[x][2] - 4.x
+		{'P','Q','R','S','T','U','V','W','X','Y','Z','[','\',']','^','_'}, //[x][3] - 5.x
+		{'`','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o'}, //[x][4] - 6.x
+		{'p','q','r','s','t','u','v','w','x','y','z','{','|','}','~','¬'}  //[x][5] - 7.x
+					 };
 
-	for (i = 0; i<MESSAGESIZE;i++)
-	{
-		message[i+j*MESSAGESIZE] = buffer[i];
-	}
+int main(){
+  int udpSocket, nBytes;
+  char buffer[1024];
+  struct sockaddr_in serverAddr, clientAddr;
+  struct sockaddr_storage serverStorage;
+  socklen_t addr_size, client_addr_size;
+  int i;
 
-		//printf("%x\n", j);
-	if (j > nextpacketnumber)
-	{
-		printf("Packet dropped, sending resend command\n");
-		return j;
-	}
+  /*Create UDP socket*/
+  udpSocket = socket(PF_INET, SOCK_DGRAM, 0);
 
-	nextpacketnumber++;
-	return 0;
-}
+  /*Configure settings in address struct*/
+  serverAddr.sin_family = AF_INET;
+  serverAddr.sin_port = htons(7891);
+  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);  
 
-void main()
-{
-    struct sockaddr_in si_me, si_other;
-     
-    int s, slen = sizeof(si_other) , recv_len;
-    char buf[PACKSIZE];
-    char message[BUFLEN];
-	char i; 
-    //create a UDP socket
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-    {
-        die("socket");
-    }
-     
-    // zero out the structure
-    memset((char *) &si_me, 0, sizeof(si_me));
-     
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(PORT);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-    //bind socket to port
-    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
-    {
-        die("bind");
-    }
-     
-    //keep listening for data
-	while (1)
-	{	
-	printf("Waiting for data...\n");    
-		while(1)
-    	{
-			memset(buf,0,PACKSIZE);
-	
-    	    //fflush(stdout);
-    	     
-    	    //try to receive some data, this is a blocking call
-    	    if ((recv_len = recvfrom(s, buf, PACKSIZE, 0, (struct sockaddr *) &si_other, &slen)) == -1)
-    	    {
-    	        die("recvfrom()");
-    	    }
-    	    if(i = convertpacket(message, buf) > 0)
-			{	
-				buf[MESSAGESIZE] = '1';  	   		
-				if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
-    	    	{
-    	    	    	die("sendto()");
-    	    	}
-			}
-			else if (i == -1)
-			{
-				buf[0] = 0;    	   		
-				if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
-    	    	{
-    	    	    	die("sendto()");
-    	    	}
-			}
-			else
-			{	
-    	    	//print details of the client/peer and the data received
-    	    	//printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
-				//printf("Recieved: Packet Number %x%x\n", (int)buf[0]-'0',(int)buf[1]-'0');
-    	    	//printf("Data: %s\n" , buf);
-    	     
-    	    	//now reply the client with the same data
-    	   		//if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == -1)
-    	    	//{
-    	    	 //   	die("sendto()");
-    	    	//}
-			}
-    	}
-	}
- 
-    close(s);
+  /*Bind socket with address struct*/
+  bind(udpSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+  /*Initialize size variable to be used later on*/
+  addr_size = sizeof serverStorage;
+
+  while(1){
+    /* Try to receive any incoming UDP datagram. Address and port of 
+      requesting client will be stored on serverStorage variable */
+    nBytes = recvfrom(udpSocket,buffer,1024,0,(struct sockaddr *)&serverStorage, &addr_size);
+
+    /*Convert message received to uppercase*/
+    for(i=0;i<nBytes-1;i++)
+      buffer[i] = toupper(buffer[i]);
+
+    /*Send uppercase message back to client, using serverStorage as the address*/
+    sendto(udpSocket,buffer,nBytes,0,(struct sockaddr *)&serverStorage,addr_size);
+  }
+
+  return 0;
 }
